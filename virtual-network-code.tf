49b4e8dcb4d0cdc3310c0f713ba79ebcd461403c
yA-005 <142383911+yA-005@users.noreply.github.com>
2024-01-26 12:31:01 +0530

    virtual-network-code.tf

--- virtual-network-code.tf
+++ virtual-network-code.tf
@@ -4,4 +4,8 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
+ tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
 }
