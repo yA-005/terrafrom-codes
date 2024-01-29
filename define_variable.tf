c2088a36588d406d9171e282571f69270b5aba77
yA-005 <142383911+yA-005@users.noreply.github.com>
2024-01-29 10:18:02 +0530

    define_variable.tf

---
+++ define_variable.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name"{
+
+    default = "myTFResourceGroup"
+
+}
+
+//TO USE THIS//
+
+esource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+//TO APPLY CHANGES//
+
+terraform apply
+
+terraform apply -var "resource_group_name=myNewResourceGroupName"  //PASSING RESOURCE GROUP NAME FROM THE COMMAND LINE//
